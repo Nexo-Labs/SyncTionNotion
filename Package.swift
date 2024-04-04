599c234755778a2e41b308ccce0030094a0a1150
Fiser <fiser12@icloud.com>
2024-04-04 23:35:20 +0200

    Renamed package

--- Package.swift
+++ Package.swift
@@ -11,13 +11,13 @@ You should have received a copy of the GNU General Public License along with thi
 import PackageDescription
 
 let package = Package(
-    name: "NotionIntegration",
+    name: "SyncTionNotion",
     platforms: [.iOS(.v16), .macOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "NotionIntegration",
-            targets: ["NotionIntegration"]),
+            name: "SyncTionNotion",
+            targets: ["SyncTionNotion"]),
     ],
     dependencies: [
         .package(url: "https://github.com/Fiser12/PreludePackage", from: "0.0.2"),
@@ -28,10 +28,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "NotionIntegration",
+            name: "SyncTionNotion",
             dependencies: ["SyncTionCore", "PreludePackage"]),
         .testTarget(
-            name: "NotionIntegrationTests",
-            dependencies: ["NotionIntegration"]),
+            name: "SyncTionNotionTests",
+            dependencies: ["SyncTionNotion"]),
     ]
 )
